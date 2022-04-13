baca7f8e0913ae536e86b138dc0b8673ce758aff
rahuldasari4 <101689375+rahuldasari4@users.noreply.github.com>
2022-04-13 09:36:20 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+	int x[10];
+	int i,n;
+	printf("Enter a value for n ");
+	scanf("%d",&n);
+	printf("Enter integer elements into array ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&x[i]);
+		if(x[i] >= 0 && x[i] <= 9)
+		printf("%d",x[i]);
+		else 
+		printf("The element you have entered is not an integer");
+	}
+	return 0;
+}
