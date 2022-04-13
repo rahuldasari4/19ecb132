9fb746f01ab0823666b0d0410719642569f98670
rahuldasari4 <101689375+rahuldasari4@users.noreply.github.com>
2022-04-13 09:32:07 +0530

    Add files via upload

---
+++ upper and lower case.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf( "enter the character in upper case ch");
+	scanf("%c",&ch);
+	ch=ch+32
+	printf("character in lower case=%c",ch);
+	return 0;
+}
