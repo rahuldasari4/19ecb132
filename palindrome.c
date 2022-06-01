6e37286d60d206f2d2d1c598ab038d593e0a2dab
rahuldasari4 <101689375+rahuldasari4@users.noreply.github.com>
2022-06-01 02:32:22 +0000

    output

--- palindrome.c
+++ palindrome.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-int main() {
-  int n, reversed = 0, remainder, original;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    original = n;
-
-    while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        n /= 10;
-    if (original == reversed)
-        printf("%d is a palindrome.", original);
-    else
-        printf("%d is not a palindrome.", original);
-
-    return 0;
-
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("palindrome number ");    
+else    
+  printf("not palindrome");   
+return 0;  
+}   
+/*output
+enter the number=4
+4 is palindrome number
+*/
